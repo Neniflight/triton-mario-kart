73281013831117c87653f199247ed27d876ad276
Calvin Nguyen <can005@ucsd.edu>
2025-08-05 20:46:14 -0700

    Added new navbar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        white: "var(--white)",
+        black: "var(--black)",
+        grey: "var(--grey)",
+        blue: "var(--blue)",
+        maroon: "var(--maroon)",
+        purple: "var(--purple)",
+        "transparent-black": "var(--transparent-black)",
+        "transparent-white": "var(--transparent-white)",
+      },
+    },
+  },
+};
